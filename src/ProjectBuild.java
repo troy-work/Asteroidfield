9b1ddd3bc71184016cb917a5bea8879c54fc2daf
Troy Cox <troycox@tcox-macbookpro.local>
2009-07-31 22:28:37 -0500

    removed files that always change

--- src/ProjectBuild.java
+++
@@ -1,8 +0,0 @@
-
-        // Automatically generated file. Do not edit.
-
-        public interface ProjectBuild {
-            public static final String VERSION = "0.3.21";
-            public static final String BUILD_NUMBER = "2806";
-        }
-        
